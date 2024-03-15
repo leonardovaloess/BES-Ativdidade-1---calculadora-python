4f1fc1cf09be48a26a04f6d6f6a9a9b9d87195bc
Leonardo Valões <leonardoberlandadevaloes@gmail.com>
2024-03-15 00:50:17 -0300

    entregando

---
+++ calc.py
@@ -0,0 +1,20 @@
+operacao = input(f"Qual operação deseja fazer?\n 1 - somar \n 2 - subtrair \n 3 - multiplicar \n 4 - dividir \n 5 - sair\n")
+
+if operacao == '1' or operacao == '2' or operacao == '3' or operacao == '4':
+    a = float(input("Informe o valor de a: "))
+    b = float(input("Informe o valor de b: "))
+
+    if operacao == '1':
+        print(f"{a} + {b} = {a + b}")
+    elif operacao == '2':
+        print(f"{a} - {b} = {a - b}")
+    elif operacao == '3':
+        print(f"{a} x {b} = {a * b}")
+    elif operacao == '4':
+        print(f"{a} : {b} = {a / b}")
+elif operacao == '5':
+    print("saindo")
+else:
+    print("Inválido")
+        
+
